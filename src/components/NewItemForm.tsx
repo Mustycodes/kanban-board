790d88ddf17c33467809d59213a394e0de730556
Mustapha Oyenuga <musty1k@yahoo.com>
2022-03-04 11:53:24 +0100

    feat:allow input to submit on press of 'enter' key

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -9,12 +9,20 @@ export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
   const inputRef = useFocus();
 
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onAdd(text);
+    }
+  };
+
   return (
     <div className="new-item-form-container">
       <input
+        type="text"
         ref={inputRef}
         value={text}
         onChange={(e) => setText(e.target.value)}
+        onKeyPress={handleAddText}
         className="new-item-input"
       />
       <button onClick={() => onAdd(text)}>Create</button>
